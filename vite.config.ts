dfb1a09408e710bddd9632e16dfbad23714a64b7
adnene <bendiouisadnane3@gmail.com>
2025-11-12 00:30:48 +0100

    fixes couple of bugs fixed a bit of responsive ness and added site map

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
+  plugins: [react(), mkcert()],
   server: {
-    https: true,
+    // Leave https empty; mkcert plugin will enable HTTPS automatically
+    port: 5173, // optional, set your local dev port
   },
-  plugins: [mkcert()],
 });
